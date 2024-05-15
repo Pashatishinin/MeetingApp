989194f2c8b1b787766187d86f0cdd40f7464fa6
PaTish <pashatishinin@gmail.com>
2024-05-15 20:57:04 +0200

    Merge pull request #4 from Pashatishinin/second
    commit

--- core/settings.py
+++ core/settings.py
@@ -124,7 +124,6 @@ USE_TZ = True
 LOGIN_URL = 'login'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
